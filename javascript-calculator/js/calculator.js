dad6e4cf931b3cdcb553a415f74da4e63f913d4c
Adam Elders <Ramor.1990@gmail.com>
2018-01-06 02:06:21 -0500

    1. Corrected/replaced some copy/paste code with function calls.
    2. Added some missing comments.
    3. Moved and grouped functions together for better readability.
    4. Added a function to add the input text to the expression text with the operator, instead of re-using the same code.
    5. Added an optional function to calculate using a floating-point precision error workaround. This fixes a rounding issue with base 2 (i.e. 0.1 + 0.2 = 0.30000000000000004). A checkbox below the calculator will toggle this option.
    6. Corrected an issue where pressing 0 would overwrite the input text rather than adding to it.
    7. Implemented the multiplication, division, and subtraction buttons.
    8. Added a psuedo-hover effect to buttons when the corresponding keys are held down.
    
    TODO:
    
    a. Implement the negate (+/-) button.
    b. Verify calculations with further testing. Only preliminary testing has been performed.
    c. Add info to the bottom of the page to explain the workaround.
    d. Add info to the bottom of the page for general use (if necessary).
    e. Prevent arithmetic operators from being executed multiple times (without any numbers being entered in between).
    f. Force rounding on result? Rounding option?
    g. Shrink input text when it starts to exceed space?

--- javascript-calculator/js/calculator.js
+++ javascript-calculator/js/calculator.js
@@ -15,6 +15,12 @@
   // Set a global variable to keep track of the decimal key.
   window.userEnteredDecimal = false;
   
+  // Set a global variable for the floating-point precision workaround
+  // if the checkbox is checked.
+  window.userCheckedWorkaround = false;
+  if (document.getElementById("workaround").checked)
+    window.userCheckedWorkaround = true;
+  
   // Adde event listeners for click events.
   document.getElementById("clearLastButton").addEventListener("click", clearLastButton);
   document.getElementById("clearAllButton").addEventListener("click", clearAllButton);
@@ -41,7 +47,13 @@
   document.getElementById("decimalButton").addEventListener("click", decimalButton);
   document.getElementById("equalsButton").addEventListener("click", equalsButton);
   
+  document.getElementById("workaround").addEventListener("change", toggleWorkaround);
+  
 })();
+
+
+/* ============================== Number Keys ============================== */
+
 
 // Define functions for button/keypresses.
 
@@ -85,11 +97,15 @@ function nineButton() {
   addNumberToInput(9);
 }
 
+
+/* ============================ Special Functions ============================ */
+
+
 // Define a function for the "CE" button.
 function clearLastButton() {
   
   // Only reset the input text.
-  document.getElementById("inputOutputText").value = 0;
+  setInputOutputValue(0);
   
   // Reset global variables.
   window.userEnteredDecimal = false;
@@ -101,8 +117,8 @@ function clearLastButton() {
 function clearAllButton() {
   
   // Reset the input text and expression text.
-  document.getElementById("inputOutputText").value = 0;
-  document.getElementById("expressionText").value = "";
+  setInputOutputValue(0);
+  setExpressionValue("");
   
   // Reset global variables.
   window.userEnteredDecimal = false;
@@ -114,7 +130,7 @@ function clearAllButton() {
 function backspaceButton() {
   
   // Get the input text.
-  var inputText = document.getElementById("inputOutputText").value;
+  var inputText = getInputOutputValue();
   
   // If the text is not default (0), remove the last character.
   // If the last character removed leaves the text blank, replace with a zero.
@@ -125,53 +141,20 @@ function backspaceButton() {
     inputText = 0;
   
   // Set the new input text.
-  document.getElementById("inputOutputText").value = inputText;
+  setInputOutputValue(inputText);
 }
 
-// Define functions for the operator buttons.
-function divideButton() {
-  
-}
-
-function multiplyButton() {
-  
-}
-
-function subtractButton() {
-  
-}
-
-function addButton() {
-  
-  // Get the input and current expression text.
-  var inputText = document.getElementById("inputOutputText").value;
-  var expressionText = document.getElementById("expressionText").value;
-  
-  // Set the user operator global.
-  window.userEnteredOperator = true;
-  
-  // If the value is not 0, add the input text and operator to the expression text.
-  if (inputText != 0) {
-    expressionText += inputText + " + ";
-    document.getElementById("expressionText").value = expressionText;
-    
-    // Reset input text.
-    document.getElementById("inputOutputText").value = 0;
-    
-    // Calculate total.
-    calculate();
-  }
-}
-
-// Functions for the "=", "+/-", and "." buttons.
+// Define a function for the "=" button.
 function equalsButton() {
   calculate(true);
 }
 
+// Define a function for the "+/-" button.
 function negativeButton() {
   
 }
 
+// Define a function for the "." button.
 function decimalButton() {
   
   // If the input is currently 0, do not add a decimal until after the second number.
@@ -194,7 +177,68 @@ function decimalButton() {
   }
 }
 
-// Function to add the number key pressed into the input/output text box.
+
+/* ============================ Operator Functions ============================ */
+
+
+// Define functions for the operator buttons.
+function divideButton() {
+  addToExpression("/");
+}
+
+function multiplyButton() {
+  addToExpression("*");
+}
+
+function subtractButton() {
+  addToExpression("-");
+}
+
+function addButton() {
+  addToExpression("+");
+}
+
+/* ====================== Evaluation/Operation Functions ====================== */
+
+// When the "use floating-point precision workaround" checkbox is 
+// changed, toggle the workaround global variable.
+function toggleWorkaround() {
+  var isChecked = document.getElementById("workaround").checked;
+  
+  window.userCheckedWorkaround = isChecked;
+}
+
+// This function is designed to work around the floating-point precision error
+// with JavaScript. Because JS has no Decimal type, you will get precision errors
+// with certain fractions like 1/10 or 0.1, because a base 2 number system
+// cannot describe these values "cleanly", and have repeating decimals.
+// In this function, each number in the expression is multiplied by 10,
+// then evaluated and divided by 10 in the calculate() function.
+function floatingPointWorkaround(expressionText) {
+  
+  // We need to split the expression text into an array.
+  var expressions = expressionText.split(" ");
+  
+  if (expressions.length > 0) {
+    
+    // We only want every other (even) array item, not the operators.
+    for (var i = 0; i < expressions.length; i += 2) {
+      expressions[i] *= 10;
+    }
+    
+    // Rebuild the array.
+    var returnExpressionText = expressions.join(" ");
+    
+    // Return the array.
+    return returnExpressionText;
+  }
+  else
+    return 0;
+}
+
+// Function to add a number key pressed into the input/output text box.
+// If the user last pressed the decimal button, append the decimal before
+// or after the input number, depending on if the input is 0.
 function addNumberToInput(numberToAdd) {
   
   // Reset input data first if the user has recently entered an operator.
@@ -217,10 +261,8 @@ function addNumberToInput(numberToAdd) {
       window.userEnteredDecimal = false;
     }
   }
-  else if (numberToAdd != 0)
-    inputText += numberToAdd;
   else
-    inputText = numberToAdd;
+    inputText += numberToAdd;
   
   // Set the new input text.
   setInputOutputValue(inputText);
@@ -229,6 +271,33 @@ function addNumberToInput(numberToAdd) {
   window.userEnteredInputValue = true;
 }
 
+// Function to add/concat the current input value to the expression text.
+// Takes the arithmetic operator as an argument.
+function addToExpression(operator) {
+  
+  // Get the input and current expression text.
+  var inputText = getInputOutputValue();
+  var expressionText = getExpressionValue();
+  
+  // Set the user operator global to true, set value entered to false.
+  window.userEnteredInputValue = false;
+  window.userEnteredOperator = true;
+  
+  // If the input value is not 0, add the input text and operator to the expression text.
+  if (inputText != 0) {
+    expressionText += inputText + " " + operator + " ";
+    setExpressionValue(expressionText);
+    
+    // Reset input text. This is to prevent calculate() from re-using the input value.
+    setInputOutputValue(0);
+    
+    // Calculate total.
+    calculate();
+  }
+}
+
+// Function to calculate the total from the expression text. Includes the input
+// text if necessary.
 function calculate(resetExpressionText) {
   
   // Get the input and expression text.
@@ -238,13 +307,23 @@ function calculate(resetExpressionText) {
   // If user recently entered a value, add the input text to the expression text.
   if (window.userEnteredInputValue)
     expressionText += inputText;
-  else {
-    // Remove trailing operator from expression text.
-    expressionText = removeTrailingOperator(expressionText);
-  }
   
-  // Evaluate the expression and print to input/output text.
-  var total = eval(expressionText);
+  // Remove trailing operator from expression text.
+  else
+    expressionText = removeTrailingOperator(expressionText);
+  
+  // If the workaround is checked, use the workaround.
+  var total;
+  if (window.userCheckedWorkaround && !window.userEnteredOperator) {
+    expressionText = floatingPointWorkaround(expressionText);
+    total = eval(expressionText);
+    total /= 10;
+  }
+  else
+    // Evaluate the expression.
+    total = eval(expressionText);
+  
+  // Write to output.
   setInputOutputValue(total);
   
   // Optionally reset the expression text (should only be set to true from the
@@ -255,6 +334,10 @@ function calculate(resetExpressionText) {
   }
 }
 
+
+/* ============================ Private Functions ============================ */
+
+
 // Functions to get or set the value of the input/output and expression text boxes.
 function getInputOutputValue() {
   return document.getElementById("inputOutputText").value;
@@ -262,7 +345,6 @@ function getInputOutputValue() {
 function getExpressionValue() {
   return document.getElementById("expressionText").value;
 }
-
 function setInputOutputValue(newValue) {
   document.getElementById("inputOutputText").value = newValue;
 }
@@ -270,6 +352,7 @@ function setExpressionValue(newValue) {
   document.getElementById("expressionText").value = newValue;
 }
 
+// Removes the trailing equation operator from the given expression text.
 function removeTrailingOperator(expressionText) {
   
   // Remove trailing operator (there should always be one).
@@ -278,80 +361,141 @@ function removeTrailingOperator(expressionText) {
   return expressionText;
 }
 
-// Use keypress event handlers for numbers and operators.
-$(window).keypress(function(e) {
+// Functions to add or remove a pseudo hover effect to buttons when keys are pressed.
+function addHoverEffect(buttonId) {
+  document.getElementById(buttonId).style.backgroundColor = "#c9c9c9";
+  document.getElementById(buttonId).style.color = "black";
+}
+function removeHoverEffect(buttonId) {
+  document.getElementById(buttonId).style.backgroundColor = "#555";
+  document.getElementById(buttonId).style.color = "white";
+}
+
+
+/* ============================ Key Event Handlers ============================ */
+
+
+// Use keydown event handlers for numbers and operators.
+$(window).keydown(function(e) {
   var key = e.which || e.keyCode;
   switch (key) {
     case 48: // 0
       zeroButton();
+      addHoverEffect("zeroButton");
       return false;
     case 49: // 1
       oneButton();
+      addHoverEffect("oneButton");
       return false;
     case 50: // 2
       twoButton();
+      addHoverEffect("twoButton");
       return false;
     case 51: // 3
       threeButton();
+      addHoverEffect("threeButton");
       return false;
     case 52: // 4
       fourButton();
+      addHoverEffect("fourButton");
       return false;
     case 53: // 5
       fiveButton();
+      addHoverEffect("fiveButton");
       return false;
     case 54: // 6
       sixButton();
+      addHoverEffect("sixButton");
       return false;
     case 55: // 7
       sevenButton();
+      addHoverEffect("sevenButton");
       return false;
     case 56: // 8
       eightButton();
+      addHoverEffect("eightButton");
       return false;
     case 57: // 9
       nineButton();
+      addHoverEffect("nineButton");
       return false;
     
     case 96: // numpad 0
       zeroButton();
+      addHoverEffect("zeroButton");
       return false;
     case 97: // numpad 1
       oneButton();
+      addHoverEffect("oneButton");
       return false;
     case 98: // numpad 2
       twoButton();
+      addHoverEffect("twoButton");
       return false;
     case 99: // numpad 3
       threeButton();
+      addHoverEffect("threeButton");
       return false;
     case 100: // numpad 4
       fourButton();
+      addHoverEffect("fourButton");
       return false;
     case 101: // numpad 5
       fiveButton();
+      addHoverEffect("fiveButton");
       return false;
     case 102: // numpad 6
       sixButton();
+      addHoverEffect("sixButton");
       return false;
     case 103: // numpad 7
       sevenButton();
+      addHoverEffect("sevenButton");
       return false;
     case 104: // numpad 8
       eightButton();
+      addHoverEffect("eightButton");
       return false;
     case 105: // numpad 9
       nineButton();
+      addHoverEffect("nineButton");
       return false;
-    
+      
+    case 8: // Backspace
+      backspaceButton();
+      addHoverEffect("backspaceButton");
+      return false;
+    case 13: // Enter
+      equalsButton();
+      addHoverEffect("equalsButton");
+      return false;
+    case 27: // Escape
+      clearAllButton();
+      addHoverEffect("clearAllButton");
+      return false;
+    case 46: // Delete
+      clearLastButton();
+      addHoverEffect("clearLastButton");
+      return false;
+    case 107: // +
+      addButton();
+      addHoverEffect("addButton");
+      return false;
+    case 110: // .
+      decimalButton();
+      addHoverEffect("decimalButton");
+      return false;
     case 106: // *
       multiplyButton();
+      addHoverEffect("multiplyButton");
       return false
     case 109: // -
       subtractButton();
+      addHoverEffect("subtractButton");
       return false;
     case 111: // /
       divideButton();
+      addHoverEffect("divideButton");
       return false;
     
     default:
@@ -359,29 +503,101 @@ $(window).keypress(function(e) {
   }
 });
 
-// Use keydown event handler for the Backspace, Esc, Del, and operator keys.
-// In Chrome, the keypress event will not trigger on these keys.
-$(document).keydown(function(e) {
-  switch(e.keyCode) {
+// Use keyup event handler to reset the button hover effect.
+$(window).keyup(function(e) {
+  var key = e.which || e.keyCode;
+  switch (key) {
+    case 48: // 0
+      removeHoverEffect("zeroButton");
+      return false;
+    case 49: // 1
+      removeHoverEffect("oneButton");
+      return false;
+    case 50: // 2
+      removeHoverEffect("twoButton");
+      return false;
+    case 51: // 3
+      removeHoverEffect("threeButton");
+      return false;
+    case 52: // 4
+      removeHoverEffect("fourButton");
+      return false;
+    case 53: // 5
+      removeHoverEffect("fiveButton");
+      return false;
+    case 54: // 6
+      removeHoverEffect("sixButton");
+      return false;
+    case 55: // 7
+      removeHoverEffect("sevenButton");
+      return false;
+    case 56: // 8
+      removeHoverEffect("eightButton");
+      return false;
+    case 57: // 9
+      removeHoverEffect("nineButton");
+      return false;
+    
+    case 96: // numpad 0
+      removeHoverEffect("zeroButton");
+      return false;
+    case 97: // numpad 1
+      removeHoverEffect("oneButton");
+      return false;
+    case 98: // numpad 2
+      removeHoverEffect("twoButton");
+      return false;
+    case 99: // numpad 3
+      removeHoverEffect("threeButton");
+      return false;
+    case 100: // numpad 4
+      removeHoverEffect("fourButton");
+      return false;
+    case 101: // numpad 5
+      removeHoverEffect("fiveButton");
+      return false;
+    case 102: // numpad 6
+      removeHoverEffect("sixButton");
+      return false;
+    case 103: // numpad 7
+      removeHoverEffect("sevenButton");
+      return false;
+    case 104: // numpad 8
+      removeHoverEffect("eightButton");
+      return false;
+    case 105: // numpad 9
+      removeHoverEffect("nineButton");
+      return false;
+      
     case 8: // Backspace
-      backspaceButton();
-      break;
+      removeHoverEffect("backspaceButton");
+      return false;
     case 13: // Enter
-      equalsButton();
-      break;
+      removeHoverEffect("equalsButton");
+      return false;
     case 27: // Escape
-      clearAllButton();
-      break;
+      removeHoverEffect("clearAllButton");
+      return false;
     case 46: // Delete
-      clearLastButton();
-      break;
+      removeHoverEffect("clearLastButton");
+      return false;
     case 107: // +
-      addButton();
-      break;
+      removeHoverEffect("addButton");
+      return false;
     case 110: // .
-      decimalButton();
-      break;
+      removeHoverEffect("decimalButton");
+      return false;
+    case 106: // *
+      removeHoverEffect("multiplyButton");
+      return false
+    case 109: // -
+      removeHoverEffect("subtractButton");
+      return false;
+    case 111: // /
+      removeHoverEffect("divideButton");
+      return false;
+    
     default:
-      break;
+      return false;      
   }
 });
